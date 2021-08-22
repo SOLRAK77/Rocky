eebf5e1c396fa0efc1ef410eac2144ab3d4b6815
SOLRAK77 <ccastillo.perez77@gmail.com>
2021-08-22 00:06:11 -0500

    add shoppingcart part I

---
+++ Rocky/Utility/EmailSender.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rocky.Utility
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            return Execute(email,subject,htmlMessage);
+        }
+
+        public async Task Execute(string email, string subject, string body)
+        {
+
+        }
+    }
+}
